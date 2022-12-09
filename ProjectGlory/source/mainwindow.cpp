ee002959915ead635acd82672d86a2fd71527152
cxy510 <13523567756@139.com>
2022-12-09 17:01:25 +0800

    在分支上进行提交

--- ProjectGlory/source/mainwindow.cpp
+++ ProjectGlory/source/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 //#include <QIcon>
-
+// branch test20221209
 MyMainWindow::MyMainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
