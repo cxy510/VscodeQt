c37e3cc896de8a3972aaf6424ba3bec6662345a0
cxy510 <13523567756@139.com>
2023-02-10 17:29:40 +0800

    增加sqlite解析单元文件

---
+++ ProjectGlory/build/subanalyzesqlite/CMakeFiles/AnalyzeSqlite.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../libAnalyzeSqlite.dll"
+  "../libAnalyzeSqlite.dll.manifest"
+  "../libAnalyzeSqlite.pdb"
+  "CMakeFiles/AnalyzeSqlite.dir/AnalyzeSqlite_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/AnalyzeSqlite.dir/AnalyzeSqlite_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/AnalyzeSqlite.dir/src/AnalyzeSqlite.cpp.obj"
+  "CMakeFiles/AnalyzeSqlite.dir/src/AnalyzeSqlite.cpp.obj.d"
+  "AnalyzeSqlite_autogen"
+  "CMakeFiles/AnalyzeSqlite_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/AnalyzeSqlite_autogen.dir/ParseCache.txt"
+  "libAnalyzeSqlite.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnalyzeSqlite.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ProjectGlory/build/subanalyzesqlite/CMakeFiles/AnalyzeSqlite_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "AnalyzeSqlite_autogen/mocs_compilation.cpp"
+  "CMakeFiles/AnalyzeSqlite_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/AnalyzeSqlite_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
