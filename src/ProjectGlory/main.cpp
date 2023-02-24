ccf12740e0bc295bc7ed477ef5d1978a9d8b25a2
cxy510 <13523567756@139.com>
2023-02-24 17:18:16 +0800

    不用导入boost库文件，可实现boost使用

--- src/ProjectGlory/main.cpp
+++ src/ProjectGlory/main.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <boost/regex.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
 
 
 int main(int argc, char *argv[])
@@ -7,5 +9,14 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MyMainWindow w;
     w.show();
+
+    std::string line("Subject: Aw shell");
+    boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
+    boost::gregorian::date d(2014, 1, 31);
+
+    boost::smatch matches;
+    if (boost::regex_match(line, matches, pat))
+        std::cout << "MATCH: " << matches[2] << std::endl;
+
     return a.exec();
 }
