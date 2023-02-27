b3c9d3252e3a1dd97c76598d3d07f0a293e991c7
cxy510 <13523567756@139.com>
2023-02-27 17:38:31 +0800

    上传mpi库

--- src/ProjectGlory/main.cpp
+++ src/ProjectGlory/main.cpp
@@ -2,7 +2,9 @@
 #include <QApplication>
 #include <boost/regex.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
-
+//#include <boost/mpi.hpp>
+#include <iostream>
+#include "mpi.h"
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +19,14 @@ int main(int argc, char *argv[])
     boost::smatch matches;
     if (boost::regex_match(line, matches, pat))
         std::cout << "MATCH: " << matches[2] << std::endl;
+
+
+    int numprocs, myid, source;
+    MPI_Status status;
+    char message[100];
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 
     return a.exec();
 }
